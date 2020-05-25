7d9bb5b0f0088c80bb1641f8aa96b8f027877b00
einzwars <einzwars@naver.com>
2020-05-25 11:09:10 +0900

    mafia agit

--- ServerScripts/MonsterAI.lua
+++ ServerScripts/MonsterAI.lua
@@ -5,7 +5,7 @@ Server.setMonsterAI(
             -- Server.SendSay('Hello, Monster AI')  -- 클라이언트에 메시지
         end
 
-        if(event == AI_UPDATE) then
+        if(event == AI_UPDATE) then             -- 2초마다 1번씩 실행
             ai.SetNearTarget(0, 200)            -- 파라미터 1번값이 0이면 플레이어 탐색, 2번값은 대상과의 거리
 
             if(ai.GetTargetUnit() ~= nil) then  -- 반환값을 주는 듯? 여하튼 여기선 반환값이 null이 아니면 실행
@@ -16,7 +16,7 @@ Server.setMonsterAI(
         end
 
         if(event == AI_ATTACKED) then           -- 적이 공격받았을 때
-            if ai.GetAttackedUnit() == nil then --공격한 유닛이 없을경우 예외처리
+            if ai.GetAttackedUnit() == nil then -- 공격한 유닛이 없을경우 예외처리
                 return
             else
                 enemy.Say('삐빅. 대상을 추적합니다.')
