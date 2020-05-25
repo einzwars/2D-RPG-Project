05ba94891828a94589f6d7b58a6dbcb7b5e2bfd5
WONWOO <einzwars@naver.com>
2020-05-26 02:07:48 +0900

    overload preview

---
+++ ServerScripts/Skill.lua
@@ -0,0 +1,15 @@
+function BasicAttack(a, b, critical)
+    local a_atk = a.atk
+    local b_def = b.def
+
+    if a_atk <= b_def then
+        return 0
+    end
+
+    if critical then
+        a_atk = a_atk*2
+    end
+
+    a.AddMP(10)
+    return a.atk
+end
